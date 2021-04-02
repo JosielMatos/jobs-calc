016aacc271626e8cebe2a8ba9fc0ada9a1d9b092
Josiel Matos <66519559+JosielMatos@users.noreply.github.com>
2021-04-02 12:00:31 -0300

    initial refactor

---
+++ src/utils/JobUtils.js
@@ -0,0 +1,15 @@
+module.exports = {
+  remainingDays(job) {
+    const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed();
+    const createdDate = new Date(job.createdAt);
+    const dueDay = createdDate.getDate() + Number(remainingDays);
+    //date in milliseconds
+    const dueDateInMs = createdDate.setDate(dueDay);
+    const timeDiffInMS = dueDateInMs - Date.now();
+    const dayInMs = 1000 * 60 * 60 * 24;
+    const dayDiff = Math.floor(timeDiffInMS / dayInMs);
+
+    return dayDiff;
+  },
+  calculateBudget: (job, valueHour) => valueHour * job["total-hours"],
+};
